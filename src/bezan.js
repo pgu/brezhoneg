9eead566fee51d701570a09585b8f7efbbde29fe
pgu <pguilcher@talend.com>
2020-01-21 15:55:23 +0100

    wip

--- src/bezan.js
+++ src/bezan.js
@@ -1,16 +1,15 @@
-import { gr_person, gr_tense, gr_number } from './grammatical-features';
+import { gr_person, gr_tense, gr_number } from "./grammatical-features";
 
 const conjugation = {
   [gr_tense.present]: {
     [gr_number.singular]: {
-      [gr_person.first]: 'on',
-      [gr_person.second]: 'out',
+      [gr_person.first]: "on",
+      [gr_person.second]: "out",
+      [gr_person.third]: "eo"
     }
   }
 };
 
-
-export function bezan({tense, number, person}) {
+export function bezan({ tense, number, person }) {
   return conjugation[tense][number][person];
 }
-
